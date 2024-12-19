da3dc1dfada68c7358e1de3b11940a4b02360e80
Akic <user.of.replit@gmail.com>
2024-12-19 11:07:03 +0100

    I cant tell but it works if flash is here

--- main.py
+++ main.py
@@ -100,6 +100,7 @@ def get_all_searches(session_id) -> list:
 @app.route("/logout")
 def logout():
     session.clear()
+    flash("You have been logged out successfully.", "success")
     return redirect(url_for("index"))
 
 
