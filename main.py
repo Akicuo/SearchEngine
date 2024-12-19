99da18a23e5f7c748a6124bf07e79e5dabeefefb
Akic <user.of.replit@gmail.com>
2024-12-19 14:31:29 +0100

    awda

--- main.py
+++ main.py
@@ -21,126 +21,26 @@ app.secret_key = "2024-BLJ-Projekt"
 
 SAE = SearchAgentEngine(API_Key="LAYLAN-01i2mdabdj3929dk2lem2l2cd1f4762e84d")
 default_pfp = "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2c/Default_pfp.svg/768px-Default_pfp.svg.png"
-connection = sql_model.Connector(
-    host="mysql2.webland.ch",
-    user="d041e_seai",
-    password="seaiSEAI$2024",
-    db="d041e_seai",
-).connect()
-
-def FindOutSubscriptionType(id):
-    global connection
-    cursor = connection.cursor()
-    cursor.execute("SELECT subscription FROM users WHERE id = %s", (id,))
-    subscription = cursor.fetchone()
-    if subscription == "free":
-        return "Free"
-    elif subscription == "premium" or subscription == None:
-        return "Premium"
-
-def FindOutUsername(id) -> str:
-    global connection
-    cursor = connection.cursor()
-    cursor.execute("SELECT username FROM users WHERE id = %s", (id,))
-    name = cursor.fetchone()
-    return name if name else None
-
-def FindOutProfileIMG(id) -> str:
-    global connection
-    cursor = connection.cursor()
-    cursor.execute("SELECT img FROM users WHERE id = %s", (id,))
-    pfp = cursor.fetchone()
-    return pfp if pfp else default_pfp
-def FindOutTimeAfterCreation(id):
-    """
-    Find out how long the user has been created for
-    """
-    global connection
-    cursor = connection.cursor()
-    cursor.execute("SELECT creation_date FROM users WHERE id = %s", (id,))
-    created_at = cursor.fetchone()
-    
-    if created_at:
-
-        if isinstance(created_at, tuple):
-            created_at = created_at[0]
-        
-
-        if not isinstance(created_at, datetime):
-            try:
-
-                created_at = datetime.strptime(str(created_at), "%Y-%m-%d %H:%M:%S")
-            except Exception as e:
-                print(f"Error converting creation date: {e}")
-                return "Unknown"
-        
-   
-        time_diff = datetime.now() - created_at
-        days = time_diff.days
-        
-        return days
-    else:
-        return "Unknown"
 
 
 
 
-def id_valid(session_id):   
-    global connection
-    cursor = connection.cursor()
-    cursor.execute("SELECT 1 FROM users WHERE id = %s", (session_id,))
-    p = cursor.fetchone()
-    if p is not None:
-        return True
-    else:
-        return False
-def add_to_searches(session_id, query) -> None:
-    global connection
-    cursor = connection.cursor()
-    cursor.execute("INSERT INTO searches (query, user_id, timestamp) VALUES (%s, %s, NOW())", (query, session_id))
-    connection.commit()
-
-def get_all_searches(session_id) -> list:
-    global connection
-    cursor = connection.cursor()
-    cursor.execute("SELECT query FROM searches WHERE user_id = %s ORDER BY timestamp DESC", (session_id,))
-    searches = cursor.fetchall()
-    return [search[0] for search in searches]
-
-
-
-@app.route("/logout")
-def logout():
-    try:
-        session["is_authenticated"] = False
-        flash("You have been logged out successfully.", "success")
-        return redirect(url_for("index"))
-    except Exception as e:
-        print(f"Error during logout: {e}")
-        flash("An error occurred during logout.", "error")
-        return render_template(redirect(url_for("index")))
 
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    user = session.get("id", None) 
+    
     session.get("username", "Guest")
     session.get("is_authenticated", False)
     session.get("isProUser", "Free")
-    if 'session_history' not in session:
-        session['session_history'] = []
+    session.get("session_history", [])
     if "is_authenticated" not in session:
             session["is_authenticated"] = False
     if "id" not in session:
         
         session["id"] = 0
         session["is_authenticated"] = False
-    """if user is not None:
-            c_user.is_authenticated = True
-            c_user.username = c_user.FindOutUsername()  
-            c_user.img_link = c_user.FindOutProfileIMG()
-            c_user.isProUser = c_user.FindOutSubscriptionType()"""
-
+    print(f"Session history: {session['session_history']}")
     if request.method == "POST":
         if request.form.get("email"):
             username = request.form.get("username")
@@ -154,12 +54,12 @@ def index():
                 return render_template("index.html", current_user=session)
 
             try:
-                cursor = connection.cursor()
+                cursor = sql_model.connection.cursor()
                 query = (
                     "INSERT INTO users (username, email, password) VALUES (%s, %s, %s)"
                 )
                 cursor.execute(query, (username, email, hashed_password))
-                connection.commit()
+                sql_model.connection.commit()
                 flash("Registration successful! You can now log in.", "success")
                 return redirect(url_for("index"))
             except Exception as e:
@@ -171,7 +71,7 @@ def index():
             hashed_password = hashlib.sha256(password.encode()).hexdigest()
 
             try:
-                cursor = connection.cursor(dictionary=True)
+                cursor = sql_model.connection.cursor(dictionary=True)
                 query = "SELECT * FROM users WHERE username = %s AND password = %s"
                 cursor.execute(query, (username, hashed_password))
 
@@ -194,14 +94,25 @@ def index():
     return render_template(
         "index.html", current_user=session )
 
+@app.route("/logout")
+def logout():
+    try:
+        session["is_authenticated"] = False
+        flash("You have been logged out successfully.", "success")
+        return redirect(url_for("index"))
+    except Exception as e:
+        print(f"Error during logout: {e}")
+        flash("An error occurred during logout.", "error")
+        return render_template(redirect(url_for("index")))
+    
 
 @app.route("/history", methods=["GET"])
 def history():
 
 
     
-    if id_valid(session_id=session.get("id", 0)):
-        server_saved_searches = get_all_searches(session_id=session["id"])
+    if sql_model.id_valid(session_id=session.get("id", 0)):
+        server_saved_searches = sql_model.get_all_searches(session_id=session["id"])
 
     return render_template("history.html", 
                            current_user=session, 
@@ -229,7 +140,8 @@ def contact():
 def profile():
     return render_template("profile.html", 
                            current_user=session, 
-                           wuf=FindOutTimeAfterCreation(session["id"]))
+                           wuf=sql_model.FindOutTimeOfCreation(session["id"]),
+                           email=sql_model.FindEmail(session["id"]))
 
 
 @app.route("/search", methods=["GET"])
@@ -238,8 +150,8 @@ def search():
 
     search_results = SAE.Search(query=query)
 
-    if id_valid(session["id"]):
-        add_to_searches(session["id"], query=query)
+    if sql_model.id_valid(session["id"]):
+        sql_model.add_to_searches(session["id"], query=query)
 
     session['session_history'].append(query)
 
--- outside_model/sql_model.py
+++ outside_model/sql_model.py
@@ -1,19 +1,75 @@
 import mysql.connector
 
-class Connector:
-    def __init__(self, host, user, password,db):
-        self.host = host
-        self.user = user
-        self.password = password
-        self.db = db
-    def connect(self):
+try:
+    connection = mysql.connector.connect(
+        host="mysql2.webland.ch",
+        user="d041e_seai",
+        password="seaiSEAI$2024",
+        db="d041e_seai",
+    )
+except mysql.connector.Error as err:
+    print(f"Something went wrong: {err}")
+else:
+    connection.connect()
 
-            connection = mysql.connector.connect(
-            host=self.host,
-            user=self.user,
-            database=self.db,
-            password=self.password,
-            )
-            return connection
+def FindOutSubscriptionType(id):
+    global connection
+    cursor = connection.cursor()
+    cursor.execute("SELECT subscription FROM users WHERE id = %s", (id,))
+    subscription = cursor.fetchone()[0]
+    if subscription == "free":
+        return "Free"
+    elif subscription == "premium" or subscription == None:
+        return "Premium"
 
+def FindOutUsername(id) -> str:
+    global connection
+    cursor = connection.cursor()
+    cursor.execute("SELECT username FROM users WHERE id = %s", (id,))
+    name = cursor.fetchone()[0]
+    return name if name else None
+
+def FindOutProfileIMG(id) -> str:
+    global connection
+    cursor = connection.cursor()
+    cursor.execute("SELECT img FROM users WHERE id = %s", (id,))
+    pfp = cursor.fetchone()[0]
+    return pfp if pfp else default_pfp
+def FindOutTimeOfCreation(id):
+    global connection
+    cursor = connection.cursor()
+    cursor.execute("SELECT creation_date FROM users WHERE id = %s", (id,))
+    created_at = cursor.fetchone()
+    return created_at[0]
+
+
+
+
+def id_valid(session_id):   
+    global connection
+    cursor = connection.cursor()
+    cursor.execute("SELECT 1 FROM users WHERE id = %s", (session_id,))
+    p = cursor.fetchone()
+    if p is not None:
+        return True
+    else:
+        return False
+def add_to_searches(session_id, query) -> None:
+    global connection
+    cursor = connection.cursor()
+    cursor.execute("INSERT INTO searches (query, user_id, timestamp) VALUES (%s, %s, NOW())", (query, session_id))
+    connection.commit()
+
+def get_all_searches(session_id) -> list:
+    global connection
+    cursor = connection.cursor()
+    cursor.execute("SELECT query FROM searches WHERE user_id = %s ORDER BY timestamp DESC", (session_id,))
+    searches = cursor.fetchall()
+    return [search[0] for search in searches]
+def FindEmail(id):
+    global connection
+    cursor = connection.cursor()
+    cursor.execute("SELECT email FROM users WHERE id = %s", (id,))
+    email = cursor.fetchone()
+    return email[0]
     
