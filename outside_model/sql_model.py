1a9b4c970a4a94172f97af06447dfc537a6651a5
Akic <user.of.replit@gmail.com>
2024-12-22 20:04:52 +0100

    im

--- outside_model/sql_model.py
+++ outside_model/sql_model.py
@@ -78,10 +78,13 @@ def Change_Profile_Picture(img_data, user_id):
     cursor = connection.cursor()
     cursor.execute("UPDATE users SET img = %s WHERE id = %s", (img_data, user_id))
     connection.commit()
-    
-def get_all_threads(session_id):
+
+def get_all_threads(session_id) -> list:
     global connection
     cursor = connection.cursor()
     cursor.execute("SELECT threads FROM searches WHERE user_id = %s ORDER BY timestamp DESC", (session_id,))
-    searches = cursor.fetchall()
-    return [search[0] for search in searches]
+    threads_selected = cursor.fetchall()
+    threads = []
+    for thread in threads_selected:
+        threads.append({"title": thread.get("title"),"id": thread.get("thread_id"), "created:_at": thread.get("thread_date")})
+    return threads                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      
